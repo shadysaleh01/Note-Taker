f0bbe4d4548653748374575911eea72c112ab265
Shady Coder <shadysaleh01@gmail.com>
2020-09-16 23:11:17 -0700

    created GET 'note' & '/' to return notes.html & index.html

--- server.js
+++ server.js
@@ -8,8 +8,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 
 
+app.get("/notes", (req, res) => {
+   res.sendFile(path.join(__dirname + "/public/notes.html"))
+})
 
+app.get("*", (req, res) => {
+   res.sendFile(path.join(__dirname + "/public/index.html"))
+})
 
+app.get("/api/notes", (req, res) => {
+   res.json("/db/db.json")
+})
 
 
 
